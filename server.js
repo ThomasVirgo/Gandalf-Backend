8d6360b19852be263c6c2cfb1dd053192d55dad0
Thomas Virgo <tomcvirgo@gmail.com>
2021-03-13 12:16:21 +0000

    updated server to match replt.it server

--- server.js
+++ server.js
@@ -2,37 +2,65 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", // will need to change to url of deployed frontend
     methods: ["GET", "POST"]
   }
 });
 
+const rooms = [];
+
 app.get('/', (req, res) => {
-  res.send('You are on the server!');
+  res.send('Server is running...');
 });
-
-
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    //broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the game');
-
-    socket.on('join game', (roomName)=>{
-      console.log('joined room: ', roomName);
-      socket.join(roomName);
+    socket.on('join game', (input)=>{
+      console.log(`${input.nickname} joined '${input.room}'`);
+      if (rooms.indexOf(input.room)!=-1){
+        socket.join(input.room);
+        socket.emit('enter room');
+        socket.to(input.room).emit('user joined', {
+          "message":`${input.nickname} has joined the game!`,
+          "nickname":input.nickname,
+          "id":socket.id
+        });
+        socket.nickname = input.nickname;
+        socket.myRoom = input.room;
+      } else {
+        socket.emit('error', 'This room has not been created yet.')
+      }
     });
 
-    socket.on('new game', (roomName)=>{
-      console.log('created room: ', roomName);
-      socket.join(roomName);
+    socket.on('new room', (input)=>{
+      console.log(`${input.nickname} created room '${input.newRoom}'`);
+      if (rooms.indexOf(input.room)==-1){
+        socket.join(input.newRoom);
+        rooms.push(input.newRoom);
+        socket.nickname = input.nickname;
+        socket.myRoom = input.newRoom;
+        socket.emit('init host')
+      } else {
+        socket.emit('error', 'This room already exists, try a different room name')
+      }
+    })
+
+    socket.on('message from client', ({message, nickname, room})=>{
+      socket.to(room).emit('chat message', {message,nickname});
+    })
+
+    socket.on('start game', (state) => {
+      socket.to(state.room).emit('game state change', state)
     })
 
     //tell all that a user disconnected
     socket.on('disconnect', () => {
-      io.emit('message', 'A user has left the game');
       console.log('user disconnected');
+      socket.to(socket.myRoom).emit('user left', {
+        "message":`${socket.nickname} has left the game.`,
+        "id":socket.id
+      })
     });
   });
 
@@ -40,4 +68,3 @@ io.on('connection', (socket) => {
 http.listen(5000, () => {
   console.log('listening on *:5000');
 });
- 
